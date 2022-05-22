3cd689b9740612ec9aecb08a2a4afee00c94c4f7
Matchobas <maalexandria@hotmail.com>
2022-05-22 17:10:27 -0300

    Add: auction must be created with at least 1 hour

--- src/useCases/CreateAuctionUseCase.ts
+++ src/useCases/CreateAuctionUseCase.ts
@@ -10,10 +10,18 @@ class CreateAuctionUseCase {
   constructor(private auctionsRepository: AuctionsRepository) {}
 
   async execute({itemName, endDate}: AuctionRequestData) {
+    // Must increase hours, but must be an automatic way
     const date = new Date(endDate);
+    date.setHours(date.getHours() + 3);
 
     if (date.getTime() < Date.now()) {
       throw new AppError('Invalid end of auction date');
+    }
+
+    const oneHourLater = new Date().setHours(date.getHours() + 1);
+
+    if (oneHourLater > date.getTime()) {
+      throw new AppError('The end must be at least one hour long from now');
     }
 
     const auction = await this.auctionsRepository.create({
