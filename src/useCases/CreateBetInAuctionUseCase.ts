ed47703c93029b303add09007cbd13723af10b79
Matchobas <maalexandria@hotmail.com>
2022-05-23 21:31:12 -0300

    Create new bet rules finished

--- src/useCases/CreateBetInAuctionUseCase.ts
+++ src/useCases/CreateBetInAuctionUseCase.ts
@@ -17,8 +17,16 @@ class CreateBetInAuctionUseCase {
   async execute({ auctionId, value, username }: BetRequestData) {
     const auction = await this.auctionsRepository.findById(auctionId);
 
+    if (value <= 0) {
+      throw new AppError("You can't bet a negative value or 0");
+    }
+
     if (!auction) {
       throw new AppError("The informed ID does not correspond to any existing auction");
+    }
+
+    if (auction.end_time < new Date()) {
+      throw new AppError("This auction already ended");
     }
 
     const bet = await this.betsRepository.create({
